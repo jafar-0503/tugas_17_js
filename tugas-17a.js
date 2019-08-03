daf63920808512e61bf4e3fc965b4d648d5514e5
Mukhamat Jafar <jafar.mukhamat7@gmail.com>
2019-08-03 23:54:08 +0700

    tugas_17_js
    tugas sudah ok !!!

---
+++ tugas-17a.js
@@ -0,0 +1,11 @@
+function ubah(){
+  var kata  = "Saya beLajar bahaSa peMrograman deNgan khUsuk";
+  var kata1 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual";
+  console.log(kata.toLowerCase());
+  console.log(kata.toUpperCase());
+  console.log("");
+  console.log(kata1.toLowerCase());
+  console.log(kata1.toUpperCase());
+}
+
+ubah();
